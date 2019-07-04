883b469569332da27c6438cd59b766d9126f8660
ibtechraj <raj_shk@rediffmail.com>
2019-07-04 20:51:20 +0530

    reddit project completed

---
+++ micro-reddit/app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+
+	validates :body, presence: true
+	validates :user_id, presence: true
+	validates :post_id, presence: true
+belongs_to :user
+belongs_to :post
+	
+end
--- micro-reddit/db/schema.rb
+++ micro-reddit/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_164007) do
+ActiveRecord::Schema.define(version: 2019_07_04_150254) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "user_id"
